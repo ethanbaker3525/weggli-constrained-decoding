9ef8ff937e2427a50fb46916bd3d5ea14ef91b6b
Ethan Baker <ethanbaker3525@gmail.com>
2024-11-13 14:05:57 -0500

    midterm report

--- test.py
+++ test.py
@@ -1,37 +1,65 @@
-import weggli
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers_cfg.grammar_utils import IncrementalGrammarConstraint, NonIncrementalGrammarConstraint
+#from transformers_cfg.generation.logits_process import GrammarConstrainedLogitsProcessor
+from logits import GrammarConstrainedLogitsProcessor, NegativeConstraintNGramLogitsProcessor
+from grammar import IncrementalTokenRecognizer as IncrementalGrammarConstraint
 
-code = """
-#include <iostream>
-#include <cstring> // for memcpy
+import logging
 
-int main() {
-    // Define a source array
-    int sourceArray[] = {10, 20, 30, 40, 50};
-    int a = 1;
-    size_t numElements = sizeof(sourceArray) / sizeof(sourceArray[0]);
-    size_t bufferSize = numElements * sizeof(int);
+#logger = logging.getLogger('test')
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="test.log",
+    encoding="utf-8",
+    filemode="a",
+    )
 
-    // Allocate a buffer with the same size as sourceArray
-    int* buffer = new int[numElements];
-    buffer[2] = buffer[3];
+if __name__ == "__main__":
 
-    // Use memcpy to copy the contents of sourceArray into the buffer
-    memcpy(buffer, sourceArray, bufferSize);
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
-    // Print the contents of the buffer to verify the copy
-    std::cout << "Buffer contents: ";
-    for (size_t i = 0; i < numElements; ++i) {
-        std::cout << buffer[i] << " ";
-    }
-    std::cout << std::endl;
+    model_id = "deepseek-ai/deepseek-coder-1.3b-base"
 
-    // Free the allocated memory
-    delete[] buffer;
+    # Load model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
 
-    return 0;
-}
-"""
+    model = AutoModelForCausalLM.from_pretrained(model_id).to(
+        device
+    )  # Load model to defined device
+    model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
-qt = weggli.parse_query("{int $a = _+foo+$a;}", False)
-print(weggli.identifiers(qt))
-print(weggli.matches(qt, "void foo() {int bar=10+foo+bar;}"))
+    # Load grammar
+    with open("json.ebnf", "r") as file:
+        grammar_str = file.read()
+    grammar = IncrementalGrammarConstraint(grammar_str, "root", tokenizer)
+    grammar_processor = GrammarConstrainedLogitsProcessor(grammar) #NegativeConstraintNGramLogitsProcessor(tokenizer(["\n"]).input_ids) #
+
+    # Generate
+    prefix1 = "This is a valid json string for http request:"
+    prefix2 = "This is a valid json string for shopping cart:"
+    input_ids = tokenizer(
+        [prefix1, prefix2], add_special_tokens=False, return_tensors="pt", padding=True
+    )["input_ids"].to(
+        device
+    )  # Move input_ids to the same device as model
+
+    output = model.generate(
+        input_ids,
+        do_sample=False,
+        max_new_tokens=60,
+        logits_processor=[grammar_processor],
+        repetition_penalty=1.1,
+        num_return_sequences=1,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    # decode output
+    generations = tokenizer.batch_decode(output, skip_special_tokens=True)
+    print(generations)
+
+    """
+    'This is a valid json string for http request:{ "request": { "method": "GET", "headers": [], "content": "Content","type": "application" }}
+    'This is a valid json string for shopping cart:This is a valid json string for shopping cart:{ "name": "MyCart", "price": 0, "value": 1 }
+    """
